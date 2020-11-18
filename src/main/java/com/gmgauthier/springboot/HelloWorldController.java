840a4bad5457556af639c937ab9f5f2569a31383
Greg Gauthier <gmgauthier@protonmail.com>
2020-11-18 23:30:34 +0000

    added properties reader, and a few other bits

--- src/main/java/com/gmgauthier/springboot/HelloWorldController.java
+++ src/main/java/com/gmgauthier/springboot/HelloWorldController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 @EnableAutoConfiguration
 public class HelloWorldController {
@@ -12,6 +15,15 @@ public class HelloWorldController {
     @RequestMapping("/")
     @ResponseBody
     String hello() {
-        return "Hello World! Spring boot is so simple.";
+        return "<h1>Hello World!</h1> \n <h2>Spring boot is so simple.</h2>";
+    }
+
+    @RequestMapping("/date")
+    @ResponseBody
+    String time() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+        String strDate = formatter.format(date);
+        return "<p><b>The Current Date is:" + strDate + "</b></p>";
     }
 }
--- src/main/java/com/gmgauthier/springboot/SpringBootWebApplication.java
+++ src/main/java/com/gmgauthier/springboot/SpringBootWebApplication.java
@@ -1,12 +1,28 @@
 package com.gmgauthier.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringBootWebApplication {
 
+    private static Logger logger = LoggerFactory.getLogger(SpringBootWebApplication.class);
+
+    @Autowired
+    private DemoProperties demoProperties;
+
     public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+
+    @PostConstruct
+    public void init() {
+
+        logger.info(demoProperties.toString());
     }
 }
